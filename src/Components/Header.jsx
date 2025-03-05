fc4ebfbc4ff882f7e3ea43a8667e78d92159fd98
AyushSinha1214 <ayushsinha1214@gmail.com>
2025-03-05 15:16:40 +0530

    updates

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import logo from './../assets/Images/logo.jpg'
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import React, { useState } from "react";
+import logo from "./../assets/Images/logo.jpg";
+import { HiMoon, HiOutlineMagnifyingGlass, HiSun } from "react-icons/hi2";
 function Header() {
+  const [toggle, setToggle] = useState(true);
   return (
-    <div>
-        <img src ={logo} width={60} height={60}/>
-        <div className='flex'>
-            <HiOutlineMagnifyingGlass/>
-            <input type="text"/>
-        </div>
+    <div className="flex items-center p-5">
+      <img src={logo} width={60} height={60} />
+      <div
+        className="flex bg-slate-200 p-2 w-full
+        mx-5 rounded-full items-center"
+      >
+        <HiOutlineMagnifyingGlass />
+        <input
+          type="text"
+          placeholder="Seacrh Games"
+          className="px-2 bg-transparent outline-none"
+        />
+      </div>
+      <div>
+        {toggle ? (
+          <HiMoon className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>setToggle(!toggle)}/>
+        ) : (
+          <HiSun className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>setToggle(!toggle)} />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
